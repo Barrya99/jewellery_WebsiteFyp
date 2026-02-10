2b373a29ae0888b4ff990085559e9c3e6451c875
Barrya99 <Bahrije.haxha@gmail.com>
2026-02-10 22:39:19 +0500

    Remove tracked virtualenv and local filepath comment; ignore venv

--- diamond-frontend/src/main.jsx
+++ diamond-frontend/src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// filepath: c:\Users\Kantesh kumar\Desktop\Jewelary_WebsiteFyp\diamond-frontend\src\main.jsx
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
